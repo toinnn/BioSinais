08d8352b99de2c51e94c05daab431f1474e20e05
Kuroro <limaalyson@hotmail.com>
2023-05-20 16:47:44 -0300

    Tokens especiais
    Tokens especiais adicionados e alguns outros detalhes pequenos

--- Ner_Models.py
+++ Ner_Models.py
@@ -1,3 +1,27 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# import matplotlib.pyplot as plt
+import random as rd
+from torch.autograd import Variable
 
+from Tener import decoderBlock
+import json
+
+CLS_Embbeding = torch.tensor( json.load(open("CLS_token.json", "r")) )
+SEP_Embbeding = torch.tensor( json.load(open("SEP_token.json", "r")) )
 print("Rodei MUUUito bem")
+
+#Este modelo consiste em 2 regressores Sigmóide e 1 classificador Softmax como OutPut :
+class Ner_Class_And_Reg(nn.Module):
+    def __init__(self,model_dim ,heads ,num_layers ,word_Embedding ,EOS , num_Classes  ,forward_expansion = 4):
+        super(decoder,self).__init__()
+        self.BOS = CLS_Embbeding
+        self.EOS = SEP_Embbeding
+
+        self.layers = nn.ModuleList( decoderBlock(model_dim , heads , forward_expansion = forward_expansion) for _ in torch.arange(num_layers))
+        
+        self.linear_Out_classes = nn.Linear(model_dim , num_Classes )
+        self.linear_Out_reg = nn.Linear(model_dim ,  2)
+
+#Este modelo consiste em 2 regressores Sigmóide  :
--- testes.py
+++ testes.py
@@ -1,5 +1,6 @@
 import torch
-from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM , GPT2Tokenizer
+import json
 
 # OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
 import logging
@@ -9,6 +10,11 @@ import matplotlib.pyplot as plt
 # % matplotlib inline
 
 # Load pre-trained model tokenizer (vocabulary)
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')#.wordpiece_tokenizer
+# tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
-print(tokenizer)
+# print(tokenizer.encode("[CLS]"))
+# print(tokenizer(["[CLS]"] , ["[CLS]"]))
+
+aux = json.load(open("CLS_token.json", "r"))
+print(torch.tensor( json.load(open("CLS_token.json", "r")) ))
