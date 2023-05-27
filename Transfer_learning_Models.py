366af4b42d714767c27ab837a8b0e86393418805
Kuroro <limaalyson@hotmail.com>
2023-05-27 19:52:51 -0300

    Update Transfer_learning_Models.py

--- Transfer_learning_Models.py
+++ Transfer_learning_Models.py
@@ -1,3 +1,11 @@
 import torchvision.models as models
+import torch.nn as nn
 
+vgg = models.vgg16(models.VGG16_Weights.IMAGENET1K_FEATURES)
+vgg.classifier = nn.Sequential(vgg.classifier[0] ,  nn.ELU())
+
+
+print(vgg.parameters)
+# for i in vgg.classifier :
+#     print(i)
 print("Transfer Learning rodado")
