b9fe2c50a9fccfe219dc3f01bfb54155b5be841d
Kuroro <limaalyson@hotmail.com>
2023-06-26 17:09:11 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import Ner_Models
+import sklearn as sk
+from sklearn.decomposition import FastICA
+
+# Ner_Models.Ner_Class_And_Reg()
+# ica = FastICA(n_components= 4)
+# signalRecont = ica.fit_transform(obsvGenerate)  # Reconstruct signals
+# mixMatrixEst = ica.mixing_  # Get estimated mixing matrix
